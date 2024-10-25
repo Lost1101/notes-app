d06f2797b07792f05c1983f2676fc6eb4d1d54fe
Lost1101 <kamilah.insani60@gmail.com>
2024-10-25 21:32:20 +0700

    test

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,29 +3,30 @@ import PropTypes from "prop-types";
 import NoteCard from './NoteCard';
 import { showFormattedDate } from '../utils/data';
 
-export default function NoteList({ title, notes, onDelete, onArchive, isArchived }) {
-  return (
-    <div className='p-5'>
-      <h1 className='text-center text-xl font-bold my-5'>{title}</h1>
-      <div className='grid grid-cols-4 gap-4'>
-        {notes
-          .filter(note => note.archived === isArchived)
-          .map(note => (
-                <NoteCard
-                    key={note.id}
-                    id={note.id}
-                    title={note.title}
-                    body={note.body}
-                    createdAt={showFormattedDate(note.createdAt)}
-                    onDelete={onDelete}
-                    onArchive={onArchive}
-                    archived={note.archived}
-                />
-          ))
-        }
-      </div>
-    </div>
-  );
+export default function NoteList({ title, notes, onDelete, onArchive, isArchived }){
+    const filteredNotes = notes.filter(note => note.archived === isArchived);
+    return (
+        <div className='p-5'>
+            <h1 className='text-center text-xl font-bold my-5'>{title}</h1>
+                <div className='grid grid-cols-4 gap-4'>
+                    {filteredNotes.length > 0 ? (
+                        filteredNotes.map(note => (
+                            <NoteCard
+                              key={note.id}
+                              id={note.id}
+                              title={note.title}
+                              body={note.body}
+                              createdAt={showFormattedDate(note.createdAt)}
+                              onDelete={onDelete}
+                              onArchive={onArchive}
+                              archived={note.archived}
+                            />
+                        ))) : (
+                      <p className="col-span-4 text-center text-gray-500">No notes found</p>
+                    )}
+                </div>
+        </div>
+    );
 };
 
 NoteList.propTypes = {
